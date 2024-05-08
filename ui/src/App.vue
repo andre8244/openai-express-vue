65f6c860e8df8d2026e36ff59d82337d76397845
Andrea Leardini <andre8244@gmail.com>
2024-05-08 18:10:25 +0200

    Add Tailwind CSS

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue'
 import { SSE } from 'sse.js'
+import { NeTextInput, NeButton } from '@nethesis/vue-components'
 
 const chatInput = ref('')
 const chatOutput = ref('')
@@ -23,9 +23,9 @@ async function send() {
   }
 
   source.addEventListener('message', (e: any) => {
-    console.log('onmessage', e)
+    // console.log('onmessage', e) ////
+    // console.log(e.data) ////
 
-    console.log(e.data) ////
     chatOutput.value += e.data
 
     // // Assuming we receive JSON-encoded data payloads: ////
@@ -41,88 +41,20 @@ async function send() {
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="p-6 space-y-6 max-w-2xl m-auto">
+    <h1 class="text-3xl font-bold text-primary-700 dark:text-primary-500">openai-express-vue</h1>
 
-    <div class="wrapper">
-      <form class="flex" @submit.prevent>
-        <div>Chat input</div>
-        <input type="text" v-model="chatInput" />
-        <button @click="send" :disabled="!chatInput">Send</button>
-        <div>Chat output</div>
-        <div>{{ chatOutput }}</div>
-      </form>
+    <form class="flex flex-col space-y-6" @submit.prevent>
+      <NeTextInput v-model.trim="chatInput" placeholder="Message" />
+      <NeButton type="submit" kind="primary" @click="send" :disabled="!chatInput">Send</NeButton>
+      <div>{{ chatOutput }}</div>
+    </form>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <!-- <nav> //// 
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav> -->
 
-  <RouterView />
+    <!-- <RouterView /> ////  -->
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
